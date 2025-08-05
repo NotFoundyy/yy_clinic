947bdb001e5d4b46515501ae8066c380b2865bc7
NotFoundyy <161956409+NotFoundyy@users.noreply.github.com>
2025-08-06 02:30:36 +0800

    Delete Clinic10.21 directory

--- Clinic10.21/Clinic/cClassQueryWin.h
+++
@@ -1,9 +0,0 @@
-#ifndef CCLASSQUERYWIN_H
-#define CCLASSQUERYWIN_H
-
-#include"cData.h"
-#include"cTool.h"
-
-int classQueryWin_init();
-
-#endif
--- Clinic10.21/Clinic/cInitWin.h
+++
@@ -1,9 +0,0 @@
-#ifndef CINITWIN_H
-#define CINITWIN_H
-
-#include"cData.h"
-#include"cTool.h"
-
-int InitWin_init();
-
-#endif
